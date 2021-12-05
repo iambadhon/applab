6fe470941a90a3897960d4bbcf3354c364b07cb0
iambadhon <badhon17890@gmail.com>
2021-12-05 23:00:51 +0600

    full complete

--- js/custom.js
+++ js/custom.js
@@ -51,6 +51,39 @@ $(function () {
         autoplaySpeed: 4000,
     });
 
+    $('.companies_slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
     //=====================testimonial end=====================//
 
 
